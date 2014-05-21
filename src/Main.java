c66636e84ffcdb04007f44dab0ef048dc5b1425c
nakakura <t.nakakura@ntt.com>
2014-05-21 13:21:21 +0900

    commands

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.io.*;
 
 public class Main {
 	public static void main(String args[]){
@@ -15,9 +16,46 @@ public class Main {
 			}
 		}
 		
-		ArrayList<String> commandList = new ArrayList<String>();
-		commandList.add("ls -al");
+		Commands command = new Commands("./command.txt");
+		ArrayList<String> commandList = command.commands();
 		SSHManager.startShell("115.69.229.18", "user", "password", commandList);
 		//SSHManager.scp("115.69.229.18", "user", "password", commandList);
+	}
+}
+
+class Commands{
+	ArrayList<String> commandList = new ArrayList<String>();
+	
+	public Commands(String filePath){
+		this._fileRead(filePath);
+	}
+	
+	private void _fileRead(String filePath) {
+	    FileReader fr = null;
+	    BufferedReader br = null;
+	    try {
+	        fr = new FileReader(filePath);
+	        br = new BufferedReader(fr);
+	 
+	        String line;
+	        while ((line = br.readLine()) != null) {
+	        	commandList.add(line);
+	        }
+	    } catch (FileNotFoundException e) {
+	        e.printStackTrace();
+	    } catch (IOException e) {
+	        e.printStackTrace();
+	    } finally {
+	        try {
+	            br.close();
+	            fr.close();
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        }
+	    }
+	}
+	
+	public ArrayList<String> commands(){
+		return commandList;
 	}
 }
